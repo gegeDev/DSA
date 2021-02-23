851eaf98ede8b70136c0c3eae7099e6a3feac894
Grzegorz Płaczek <gegepl123@gmail.com>
2021-02-23 14:52:08 +0100

    improved heap, added priority queue functionality

--- heap.cpp
+++ heap.cpp
@@ -8,6 +8,7 @@ using namespace std;
 class Heap{
     int array[maxSize];
     int last = 0;
+    void fix(int);
 public:
     Heap(vector<int> &a){
         for(int i = 0; i < a.size(); i++){
@@ -17,10 +18,28 @@ public:
     void insert(int);
     void show();
     void hSort();
+    int pop();
 };
 
 int main(){
-    vector<int> tab;
+    int n;
+    vector<int> t;
+    Heap h1(t);
+    while(true){
+        cout << "1. insert" << endl << "2. pop" << endl << "3. hSort" << endl;
+        cin >> n;
+        if(n == 1){
+            cin >> n;
+            h1.insert(n);
+        }else if(n == 2){
+            n = h1.pop();
+            cout << n << endl;
+        }else if(n == 3){
+            h1.hSort();
+        }
+        h1.show();
+    }
+    /*vector<int> tab;
     clock_t start, stop;
     srand(time(NULL));
     for(int i = 0; i < maxSize; i++){
@@ -38,7 +57,7 @@ int main(){
     start = clock();
     sort(tab.begin(), tab.end());
     stop = clock();
-    cout << (double)(stop - start) / CLOCKS_PER_SEC << endl;
+    cout << (double)(stop - start) / CLOCKS_PER_SEC << endl;*/
     return 0;
 }
 
@@ -59,18 +78,28 @@ void Heap::show(){
 
 void Heap::hSort(){
     int temp = last;
-    int i;
     for(int x = 0; x < last - 1; x++){
         swap(array[0], array[--temp]);
-        i = 0;
-        while(2 * i + 1 < temp){
-            if(2 * i + 2 < temp && array[2 * i + 2] > array[2 * i + 1] && array[2 * i + 2] > array[i]){
-                swap(array[i], array[2 * i + 2]);
-                i = 2 * i + 2;
-            }else if(array[2 * i + 1] > array[i]){
-                swap(array[2 * i + 1], array[i]);
-                i = 2 * i + 1;
-            }else{break;}
-        }
+        fix(temp);
     }
+}
+
+void Heap::fix(int l){
+    int i = 0;
+    while(2 * i + 1 < l){
+        if(2 * i + 2 < l && array[2 * i + 2] > array[2 * i + 1] && array[2 * i + 2] > array[i]){
+            swap(array[i], array[2 * i + 2]);
+            i = 2 * i + 2;
+        }else if(array[2 * i + 1] > array[i]){
+            swap(array[2 * i + 1], array[i]);
+            i = 2 * i + 1;
+        }else{break;}
+    }
+}
+
+int Heap::pop(){
+    int e = array[0];
+    swap(array[0], array[--last]);
+    fix(last);
+    return e;
 }
